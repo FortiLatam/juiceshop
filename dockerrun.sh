e09ca720068199bd8e49416c929572d6bdb564c5
FortiLatam <97236287+FortiLatam@users.noreply.github.com>
2024-10-31 12:02:23 -0300

    Create dockerrun.sh

---
+++ dockerrun.sh
@@ -0,0 +1,5 @@
+python3 -m pip install selenium webdriver_manager
+wget https://raw.githubusercontent.com/FortiLatam/DVWASummit23/refs/heads/master/docs/simple_ff.py
+wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.35.0-linux64.tar.gz 
+mv geckodriver /usr/local/bin/
