00f7592abbf0a0b2eec0fc9b17615cf779a8c89c
FortiLatam <97236287+FortiLatam@users.noreply.github.com>
2025-03-17 18:52:15 -0300

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,6 @@
 python3 -m pip install selenium webdriver_manager
 wget https://raw.githubusercontent.com/FortiLatam/DVWASummit23/refs/heads/master/docs/simple_ff.py
-wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
-tar -xvzf geckodriver-v0.35.0-linux64.tar.gz 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.35.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.36.0-linux64.tar.gz 
 mv geckodriver /usr/local/bin/
+xrandr --output DUMMY0 --mode 1920x1080
